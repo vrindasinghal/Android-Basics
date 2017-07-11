9b1a5ddb5cedeaa804f8606d7c76b000754b8c9c
vrindasinghal <vrinda74835@gmail.com>
2017-07-11 22:31:20 +0530

    11 July'17

---
+++ JavaMailAPIEmailSending/app/src/main/java/com/example/android/javamailapiemailsending/Config.java
@@ -0,0 +1,10 @@
+package com.example.android.javamailapiemailsending;
+
+/**
+ * Created by vrinda on 11-07-2017.
+ */
+
+public class Config {
+    public static final String EMAIL = "vrinda74835@gmail.com";
+    public static final String PASSWORD = "password";
+}
